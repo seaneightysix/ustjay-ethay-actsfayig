b3366ef164616ef01ba85c4d1771cdaec1310e28
seaneightysix <sean.eightysix@GMAIL.com>
2019-07-21 11:38:45 -0500

    completed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 import requests
-from flask import Flask, send_file, Response
+from flask import Flask, send_file, Response, redirect, render_template
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
@@ -16,11 +16,23 @@ def get_fact():
 
     return facts[0].getText()
 
+def get_pig_latin(fact):
 
-@app.route('/')
+    url = "https://hidden-journey-62459.herokuapp.com/piglatinize/"
+    data = {"input_text": fact}
+    res = requests.post(url, data, allow_redirects=False)
+    link = res.headers["Location"]
+
+    return link
+
+@app.route('/', methods = ['GET'])
 def home():
-    return "FILL ME!"
 
+    fact = get_fact()
+    link = get_pig_latin(fact)
+
+    return render_template('home.jinja2', fact=fact, link=link)
+    #return redirect(link)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
